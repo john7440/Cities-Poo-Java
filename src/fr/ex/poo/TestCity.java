37c5a5cace594af1fa61bb23cfae796cdf66e021
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 11:34:51 +0100

    mise aupropre des commentaires

--- src/fr/ex/poo/TestCity.java
+++ src/fr/ex/poo/TestCity.java
@@ -4,23 +4,21 @@ public class TestCity {
 
 	public static void main(String[] args) {
 		
-		// test instanciations
+		// test instance of different cities
 		City Toulouse = new City("France", "Toulouse", 450000);
 		City Annecy = new City("France", "Annecy", 131000);
 		City Rabat = new City("Rabat", 577000);
 		
-		
-		//display
+		// Test display method
 		Toulouse.display();
 		Annecy.display();
 		Rabat.display();
 		
-		
+		// Test print method
 		System.out.println(Toulouse);
 		
-		System.out.println(Rabat.getId());
-		
-		
+		// Test ID 
+		System.out.println(Rabat.getId());	
 
 	}
 
