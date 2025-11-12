bb8b8ef2efb37815f8a6c0b821d27174ee8c138e
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 10:55:09 +0100

    test pour questions 1.3

--- src/fr/ex/poo/TestCity.java
+++ src/fr/ex/poo/TestCity.java
@@ -8,9 +8,12 @@ public class TestCity {
 		City Toulouse = new City("France", "Toulouse", 450000);
 		City Annecy = new City("France", "Annecy", 131000);
 		
+		City Rabat = new City("Rabat", 577000);
+		
 		//display
 		Toulouse.display();
 		Annecy.display();
+		Rabat.display();
 		
 		//test changements 
 		Toulouse.setNumberOfCitizen(370000);
