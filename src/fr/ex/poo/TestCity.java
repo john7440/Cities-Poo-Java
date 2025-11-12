aa8cc1be7e1d2990b4294b73180419ca1b69574b
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 11:23:30 +0100

    ajout de test pour question 1.8 avec l'id

--- src/fr/ex/poo/TestCity.java
+++ src/fr/ex/poo/TestCity.java
@@ -7,18 +7,18 @@ public class TestCity {
 		// test instanciations
 		City Toulouse = new City("France", "Toulouse", 450000);
 		City Annecy = new City("France", "Annecy", 131000);
-		
 		City Rabat = new City("Rabat", 577000);
+		
 		
 		//display
 		Toulouse.display();
 		Annecy.display();
 		Rabat.display();
 		
-		//test changements 
-		Toulouse.setNumberOfCitizen(370000);
 		
 		System.out.println(Toulouse);
+		
+		System.out.println(Rabat.getId());
 		
 		
 
